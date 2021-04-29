9109e0b43e2c8623c9750bd042e62740d343b781
njuta00 <>
2021-04-29 07:36:44 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int m, k, counter=0;
+  cout << "Ievadiet skaitļus m>k";
+  cin >> m >> k;
+  for (int i=1; i<=m; i++) 
+  {
+    if (i%k==0) 
+    {counter++;}
+  }
+  cout << counter;
+  return 0;
+
+}
